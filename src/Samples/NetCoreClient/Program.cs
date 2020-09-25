b87f45087e5c4f8df69d20399dc0827a574e3666
Fred am Nil <maxdome@vollbio.de>
2020-09-25 10:27:46 +0200

    Certificate added

--- src/Samples/NetCoreClient/Program.cs
+++ src/Samples/NetCoreClient/Program.cs
@@ -1,43 +1,48 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Net;
+﻿using System;
 using System.ServiceModel;
-using System.Text;
 
 namespace NetCoreClient
 {
-    class Program
+  internal class Program
+  {
+    //private readonly static string _basicHttpEndPointAddress = @"http://localhost:8080/basichttp";
+    private readonly static string _soapEnvelopeContent = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"><soapenv:Body><Echo xmlns = \"http://tempuri.org/\" ><text>Hello</text></Echo></soapenv:Body></soapenv:Envelope>";
+
+    private static void Main(string[] args)
     {
-        //private readonly static string _basicHttpEndPointAddress = @"http://localhost:8080/basichttp";
-        private readonly static string _soapEnvelopeContent = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"><soapenv:Body><Echo xmlns = \"http://tempuri.org/\" ><text>Hello</text></Echo></soapenv:Body></soapenv:Envelope>";
+      CallUsingWcf();
+      //  CallUsingWebRequest();
 
+      Console.WriteLine("Hit enter to exit");
+      Console.ReadLine();
+    }
 
-        static void Main(string[] args)
-        {
-            CallUsingWcf();
-          //  CallUsingWebRequest();
+    private static void CallUsingWcf()
+    {
+      /*var factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
+      factory.Open();
+      var channel = factory.CreateChannel();
+      ((IClientChannel)channel).Open();
+      Console.WriteLine("http Echo(\"Hello\") => " + channel.Echo("Hello"));
+      ((IClientChannel)channel).Close();
+      factory.Close();*/
 
-            Console.WriteLine("Hit enter to exit");
-            Console.ReadLine();
-        }
+      ChannelFactory<Contract.IEchoService> factory = new ChannelFactory<Contract.IEchoService>(CreateBinding(), new EndpointAddress("net.tcp://localhost:800/nettcp"));
+      factory.Open();
+      Contract.IEchoService channel = factory.CreateChannel();
+      ((IClientChannel)channel).Open();
+      Console.WriteLine("net.tcp Echo(\"Hello\") => " + channel.Echo("Hello"));
+      ((IClientChannel)channel).Close();
+      factory.Close();
 
-        private static void CallUsingWcf()
-        {
-            /*var factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
-            factory.Open();
-            var channel = factory.CreateChannel();
-            ((IClientChannel)channel).Open();
-            Console.WriteLine("http Echo(\"Hello\") => " + channel.Echo("Hello"));
-            ((IClientChannel)channel).Close();
-            factory.Close();*/
+      static NetTcpBinding CreateBinding()
+      {
+        NetTcpBinding result = new NetTcpBinding();
+        result.Security.Mode = SecurityMode.Transport;
+        result.Security.Transport.ClientCredentialType = TcpClientCredentialType.None;
+        return result;
+      }
 
-            var factory = new ChannelFactory<Contract.IEchoService>(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:800/nettcp"));
-            factory.Open();
-            var channel = factory.CreateChannel();
-            ((IClientChannel)channel).Open();
-            Console.WriteLine("net.tcp Echo(\"Hello\") => " + channel.Echo("Hello"));
-            ((IClientChannel)channel).Close();
-            factory.Close();
       /*
             // Complex type testing
             factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
@@ -47,39 +52,39 @@ namespace NetCoreClient
             Console.WriteLine("http EchoMessage(\"Complex Hello\") => " + channel.ComplexEcho(new Contract.EchoMessage() { Text = "Complex Hello" }));
             ((IClientChannel)channel).Close();
             factory.Close();*/
-        }
-/*
-        private static void CallUsingWebRequest() 
-        {
-            // 
-            // The following sample, creates a basic web request to the specified endpoint, sends the SOAP request and reads the response
-            // 
-
-            // Prepare the raw content
-            var utf8Encoder = new UTF8Encoding();
-            var bodyContentBytes = utf8Encoder.GetBytes(_soapEnvelopeContent);
-
-            // Create the web request
-            var webRequest = System.Net.WebRequest.Create(new Uri(_basicHttpEndPointAddress));
-            webRequest.Headers.Add("SOAPAction", "http://tempuri.org/IEchoService/Echo");
-            webRequest.ContentType = "text/xml";
-            webRequest.Method = "POST";
-            webRequest.ContentLength = bodyContentBytes.Length;
-
-            // Append the content
-            var requestContentStream = webRequest.GetRequestStream();
-            requestContentStream.Write(bodyContentBytes, 0, bodyContentBytes.Length);
-
-            // Send the request and read the response
-            using (System.IO.Stream responseStream = webRequest.GetResponse().GetResponseStream())
+    }
+    /*
+            private static void CallUsingWebRequest() 
             {
-                using (System.IO.StreamReader responsereader = new System.IO.StreamReader(responseStream))
+                // 
+                // The following sample, creates a basic web request to the specified endpoint, sends the SOAP request and reads the response
+                // 
+
+                // Prepare the raw content
+                var utf8Encoder = new UTF8Encoding();
+                var bodyContentBytes = utf8Encoder.GetBytes(_soapEnvelopeContent);
+
+                // Create the web request
+                var webRequest = System.Net.WebRequest.Create(new Uri(_basicHttpEndPointAddress));
+                webRequest.Headers.Add("SOAPAction", "http://tempuri.org/IEchoService/Echo");
+                webRequest.ContentType = "text/xml";
+                webRequest.Method = "POST";
+                webRequest.ContentLength = bodyContentBytes.Length;
+
+                // Append the content
+                var requestContentStream = webRequest.GetRequestStream();
+                requestContentStream.Write(bodyContentBytes, 0, bodyContentBytes.Length);
+
+                // Send the request and read the response
+                using (System.IO.Stream responseStream = webRequest.GetResponse().GetResponseStream())
                 {
-                    var soapResponse = responsereader.ReadToEnd();
-                    Console.WriteLine($"Http SOAP Response: {soapResponse}");
+                    using (System.IO.StreamReader responsereader = new System.IO.StreamReader(responseStream))
+                    {
+                        var soapResponse = responsereader.ReadToEnd();
+                        Console.WriteLine($"Http SOAP Response: {soapResponse}");
+                    }
                 }
             }
-        }
-*/
-    }
+    */
+  }
 }
--- src/Samples/NetCoreServer/Startup.cs
+++ src/Samples/NetCoreServer/Startup.cs
@@ -1,4 +1,5 @@
-﻿using CoreWCF;
+﻿using System.Security.Cryptography.X509Certificates;
+using CoreWCF;
 using CoreWCF.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,10 +9,7 @@ namespace NetCoreServer
 {
   public class Startup
   {
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddServiceModelServices();
-    }
+    public void ConfigureServices(IServiceCollection services) => services.AddServiceModelServices();
 
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
@@ -19,9 +17,20 @@ namespace NetCoreServer
       {
         builder
             .AddService<EchoService>()
-            .AddServiceEndpoint<EchoService, Contract.IEchoService>(new BasicHttpBinding(), "/basichttp")
-            .AddServiceEndpoint<EchoService, Contract.IEchoService>(new NetTcpBinding(), "/nettcp");
+            //            .AddServiceEndpoint<EchoService, Contract.IEchoService>(new BasicHttpBinding(), "/basichttp")
+            .AddServiceEndpoint<EchoService, Contract.IEchoService>(CreateBinding(), "/nettcp")
+            .ConfigureServiceHostBase<EchoService>(serviceHost =>
+          serviceHost.Credentials.ServiceCertificate.SetCertificate(StoreLocation.LocalMachine,
+            StoreName.My, X509FindType.FindByIssuerName, "KOMSA ISSUING CA"));
       });
+
+      static NetTcpBinding CreateBinding()
+      {
+        NetTcpBinding result = new NetTcpBinding();
+        result.Security.Mode = SecurityMode.Transport;
+        result.Security.Transport.ClientCredentialType = TcpClientCredentialType.None;
+        return result;
+      }
     }
   }
 }
