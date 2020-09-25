5090d400b239f3fff9331d350002e6b06c667606
Fred am Nil <maxdome@vollbio.de>
2020-09-25 09:58:44 +0200

    Merge-Konflikt repariert

--- src/CoreWCF.Primitives/src/CoreWCF/Configuration/ServiceBuilder.cs
+++ src/CoreWCF.Primitives/src/CoreWCF/Configuration/ServiceBuilder.cs
@@ -5,135 +5,120 @@ using System.Collections.Generic;
 
 namespace CoreWCF.Configuration
 {
-    internal class ServiceBuilder : IServiceBuilder
+  internal class ServiceBuilder : IServiceBuilder
+  {
+    private IServiceProvider _serviceProvider;
+    private IDictionary<Type, IServiceConfiguration> _services = new Dictionary<Type, IServiceConfiguration>();
+
+    public ServiceBuilder(IServiceProvider serviceProvider)
     {
-        private IServiceProvider _serviceProvider;
-        private IDictionary<Type, IServiceConfiguration> _services = new Dictionary<Type, IServiceConfiguration>();
-
-        public ServiceBuilder(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-
-        public ICollection<IServiceConfiguration> ServiceConfigurations => _services.Values;
-
-        public ICollection<Uri> BaseAddresses { get; } = new List<Uri>();
-
-        ICollection<Type> IServiceBuilder.Services => _services.Keys;
-
-        public IServiceProvider ServiceProvider => _serviceProvider;
-
-        public IServiceBuilder AddService<TService>() where TService : class
-        {
-<<<<<<< HEAD
-            AddService(typeof(TService));
-        }
-
-        public void AddService(Type service)
-=======
-            return AddService(typeof(TService));
-        }
-
-        public IServiceBuilder AddService(Type service)
->>>>>>> master
-        {
-            if (service is null)
-            {
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(service)));
-            }
-            var serviceConfig = (IServiceConfiguration)_serviceProvider.GetRequiredService(
-                typeof(IServiceConfiguration<>).MakeGenericType(service));
-            _services[serviceConfig.ServiceType] = serviceConfig;
-            return this;
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, string address)
-        {
-            return AddServiceEndpoint<TService>(typeof(TContract), binding, address);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, Uri address)
-        {
-            return AddServiceEndpoint<TService>(typeof(TContract), binding, address);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, string address, Uri listenUri)
-        {
-            return AddServiceEndpoint<TService>(typeof(TContract), binding, address, listenUri);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, Uri address, Uri listenUri)
-        {
-            return AddServiceEndpoint<TService>(typeof(TContract), binding, address, listenUri);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address)
-        {
-            return AddServiceEndpoint<TService>(implementedContract, binding, address, (Uri)null);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address)
-        {
-            return AddServiceEndpoint<TService>(implementedContract, binding, address, (Uri)null);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address, Uri listenUri)
-        {
-            if (address is null)
-            {
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(address)));
-            }
-
-            return AddServiceEndpoint<TService>(implementedContract, binding, new Uri(address, UriKind.RelativeOrAbsolute), listenUri);
-        }
-
-        public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address, Uri listenUri)
-        {
-            return AddServiceEndpoint(typeof(TService), implementedContract, binding, address, listenUri);
-        }
-
-        public IServiceBuilder AddServiceEndpoint(Type service, Type implementedContract, Binding binding, Uri address, Uri listenUri)
-        {
-<<<<<<< HEAD
-            AddServiceEndpoint(typeof(TService), implementedContract, binding, address, listenUri);
-        }
-
-        public void AddServiceEndpoint(Type service, Type implementedContract, Binding binding, Uri address, Uri listenUri)
-        {
-=======
->>>>>>> master
-            if (service is null)
-            {
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(service)));
-            }
-
-            if (implementedContract is null)
-            {
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(implementedContract)));
-            }
-
-            if (binding is null)
-            {
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(binding)));
-            }
-
-            if (_services.TryGetValue(service, out IServiceConfiguration serviceConfig))
-            {
-                serviceConfig.Endpoints.Add(new ServiceEndpointConfiguration()
-                {
-                    Address = address,
-                    Binding = binding,
-                    Contract = implementedContract,
-                    ListenUri = listenUri
-                });
-            }
-            else
-            {
-                // TODO: Either find an existing SR to use or create a new one.
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentException(nameof(service)));
-            }
-
-            return this;
-        }
+      _serviceProvider = serviceProvider;
     }
+
+    public ICollection<IServiceConfiguration> ServiceConfigurations => _services.Values;
+
+    public ICollection<Uri> BaseAddresses { get; } = new List<Uri>();
+
+    ICollection<Type> IServiceBuilder.Services => _services.Keys;
+
+    public IServiceProvider ServiceProvider => _serviceProvider;
+
+    public IServiceBuilder AddService<TService>() where TService : class
+    {
+      return AddService(typeof(TService));
+    }
+
+    public IServiceBuilder AddService(Type service)
+    {
+      if (service is null)
+      {
+        throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(service)));
+      }
+      var serviceConfig = (IServiceConfiguration)_serviceProvider.GetRequiredService(
+          typeof(IServiceConfiguration<>).MakeGenericType(service));
+      _services[serviceConfig.ServiceType] = serviceConfig;
+      return this;
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, string address)
+    {
+      return AddServiceEndpoint<TService>(typeof(TContract), binding, address);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, Uri address)
+    {
+      return AddServiceEndpoint<TService>(typeof(TContract), binding, address);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, string address, Uri listenUri)
+    {
+      return AddServiceEndpoint<TService>(typeof(TContract), binding, address, listenUri);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService, TContract>(Binding binding, Uri address, Uri listenUri)
+    {
+      return AddServiceEndpoint<TService>(typeof(TContract), binding, address, listenUri);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address)
+    {
+      return AddServiceEndpoint<TService>(implementedContract, binding, address, (Uri)null);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address)
+    {
+      return AddServiceEndpoint<TService>(implementedContract, binding, address, (Uri)null);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, string address, Uri listenUri)
+    {
+      if (address is null)
+      {
+        throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(address)));
+      }
+
+      return AddServiceEndpoint<TService>(implementedContract, binding, new Uri(address, UriKind.RelativeOrAbsolute), listenUri);
+    }
+
+    public IServiceBuilder AddServiceEndpoint<TService>(Type implementedContract, Binding binding, Uri address, Uri listenUri)
+    {
+      return AddServiceEndpoint(typeof(TService), implementedContract, binding, address, listenUri);
+    }
+
+    public IServiceBuilder AddServiceEndpoint(Type service, Type implementedContract, Binding binding, Uri address, Uri listenUri)
+    {
+      if (service is null)
+      {
+        throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(service)));
+      }
+
+      if (implementedContract is null)
+      {
+        throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(implementedContract)));
+      }
+
+      if (binding is null)
+      {
+        throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentNullException(nameof(binding)));
+      }
+
+      if (_services.TryGetValue(service, out IServiceConfiguration serviceConfig))
+      {
+        serviceConfig.Endpoints.Add(new ServiceEndpointConfiguration()
+        {
+          Address = address,
+          Binding = binding,
+          Contract = implementedContract,
+          ListenUri = listenUri
+        });
+      }
+      else
+      {
+        // TODO: Either find an existing SR to use or create a new one.
+        throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new ArgumentException(nameof(service)));
+      }
+
+      return this;
+    }
+  }
 }
--- src/Samples/NetCoreServer/Startup.cs
+++ src/Samples/NetCoreServer/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CoreWCF;
+﻿using CoreWCF;
 using CoreWCF.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -7,32 +6,22 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace NetCoreServer
 {
-    public class Startup
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddServiceModelServices();
-        }
-        
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseServiceModel(builder =>
-            {
-<<<<<<< HEAD
-/*
-                builder.AddService<EchoService>();
-                builder.AddServiceEndpoint<EchoService, Contract.IEchoService>(new BasicHttpBinding(), "/basichttp");
-                builder.AddServiceEndpoint<EchoService, Contract.IEchoService>(new NetTcpBinding(), "/nettcp");
-*/
-                builder.AddService(typeof(EchoService));
-                builder.AddServiceEndpoint(typeof(EchoService), typeof(Contract.IEchoService), new NetTcpBinding(), new Uri("/nettcp", UriKind.Relative), null);
-=======
-                builder
-                    .AddService<EchoService>()
-                    .AddServiceEndpoint<EchoService, Contract.IEchoService>(new BasicHttpBinding(), "/basichttp")
-                    .AddServiceEndpoint<EchoService, Contract.IEchoService>(new NetTcpBinding(), "/nettcp");
->>>>>>> master
-            });
-        }
+      services.AddServiceModelServices();
     }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      app.UseServiceModel(builder =>
+      {
+        builder
+            .AddService<EchoService>()
+            .AddServiceEndpoint<EchoService, Contract.IEchoService>(new BasicHttpBinding(), "/basichttp")
+            .AddServiceEndpoint<EchoService, Contract.IEchoService>(new NetTcpBinding(), "/nettcp");
+      });
+    }
+  }
 }
